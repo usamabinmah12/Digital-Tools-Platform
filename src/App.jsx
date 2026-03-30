f28e706b67cfbf22a2e6e5ee211f342222816b44
usamabinmah12 <usamabinmahbub13@gmail.com>
2026-03-30 10:36:54 +0600

    Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { ToastContainer } from 'react-toastify'
+import './App.css'
+import Navbar from './components/Navbar'
+// import Banner from './components/Banner'
+// import Stat from './components/Stat'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <Navbar></Navbar>
+      {/* <Banner></Banner>
+      <Stat></Stat> */}
+    </>
+  )
+}
+
+export default App
